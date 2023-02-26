b3d4a6b33070988238c9509586e98e3a3dc8974b
Mathilde <mail.mathildelafay@gmail.com>
2023-02-26 17:52:16 +0100

    Create euler.c

---
+++ euler.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+
+long long euler11(int grid[20][]
+{
+    
+})
+
+int main()
+{
+    long long grid[20][20];
+    printf("\nréponse : %d\n", euler11());
+}
