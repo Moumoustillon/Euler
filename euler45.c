92c83e2f3f0fddb384700634cd4587ae304d18f6
Mathilde <mail.mathildelafay@gmail.com>
2023-04-15 00:40:14 +0200

    sss

--- euler45.c
+++ euler45.c
@@ -3,6 +3,11 @@
 #include<math.h>
 #include<time.h>
 
+unsigned long int generate_triangular(unsigned long int n)
+{
+    return n*(2*n-1);
+}
+
 int isPentagonal(long long int num) {
     // A number is part of the pentagonal sequence if and only if
     // it can be expressed as n(3n-1)/2 for some positive integer n
@@ -15,27 +20,40 @@ int isPentagonal(long long int num) {
     }
     return 0;
 }
-int isHexagonal(long long num) {
+
+int isHexagonal(unsigned long int num) {
     // A number is part of the hexagonal sequence if and only if
     // it can be expressed as n(2n-1) for some positive integer n
     // We can rearrange this formula to check if (sqrt(8*num + 1) + 1) / 4 is an integer
-    long long root = sqrt(8 * num + 1);
+    unsigned long int root = sqrt(8 * num + 1);
     if (((int)root == root) && ((int)root % 4 == 3))
     {
         return 1;
     }
     return 0;
 }
-
-
+//27694
+void euler45()
+{
+    unsigned long int i, t;
+    for (i=144;i>0;i++)
+    {
+        t = generate_triangular(i);
+        printf("%llu, %llu\n", i, t);
+        if (isPentagonal(t)==1)
+        {
+            printf("%llu\n", t);
+            break;
+        }
+    }
+}
 
 int main()
 {
     clock_t start, end;
     start = clock();
-    int n = 1532447841;
-    printf("%d\n", isPentagonal(n));
-    printf("%d\n", isHexagonal(n));
+
+    euler45();
 
     end = clock();
     double duration = ((double)end - start)/CLOCKS_PER_SEC;
