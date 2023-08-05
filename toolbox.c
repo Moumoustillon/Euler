0f48a2691b964ea688162f809b8f44e11a129ab8
Mathilde <mail.mathildelafay@gmail.com>
2023-08-05 13:13:36 +0200

    g

--- toolbox.c
+++ toolbox.c
@@ -1,18 +1,9 @@
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
 #include"toolbox.h"
 
-
-int dichotomy(int array[], int a, int b, int target)
-{
-    int m = (a+b)/2;
-    if (a>b) return -1;
-    if (array[m]>target) return dichotomy(array, a, m-1, target);
-    if (array[m]<target) return dichotomy(array, m+1, b, target);
-    return m;
-}
+// int, numbers and calculations ...
 
 int max(int a, int b)
 {
@@ -47,31 +38,6 @@ int count_digits(int n)
     return digits;
 }
 
-void display_array(int array[], int size)
-{
-    int i;
-    printf("{");
-    for (i=0;i<size;i++)
-    {
-        printf("%d,", array[i]);
-    }
-    printf("}\n");
-}
-
-void display_array_as_number(int array[], int size)
-{
-    int i;
-    for (i=0;i<size;i++)
-    {
-        printf("%d", array[i]);
-    }
-    printf("\n");
-}
-
-int is_a_permutation_of_b(int a, int b)
-{
-    
-}
 int factorial(int n)
 {
     int i, fact = 1;
@@ -82,7 +48,51 @@ int factorial(int n)
     return fact;
 }
 
-int permutation_ab(int a, int b)
+int power(int x, int n)
+{
+    if (n<0) return n;
+    if (n==0) return 1;
+    if (n%2 == 0) return power(x*x, n/2);
+    return x*power(x*x,(n-1)/2);
+}
+
+// float
+
+float power_f(float x, int n)
+{
+    if (n<0)
+    {
+        if (x == 0) return x; 
+        x = 1/x;
+        return power_f(x, -n);
+    }
+    if (n==0) return 1;
+    if (n%2 == 0) return power_f(x*x, n/2);
+    return x*power_f(x*x,(n-1)/2);
+}
+
+// number analysis functions
+
+int is_n_palindromic(int n)
+{
+    int i, r = 0, copy;
+    copy = n;
+
+    while(copy!=0)
+    {
+        r = r*10;
+        r = r+(copy%10);
+        copy = copy/10;
+    }
+    if(r == n) 
+    {
+        printf("pal %d\n", n);
+        return 1;
+    }
+    return 0;
+}
+
+int is_a_permutation_of_b(int a, int b)
 {
     int digits[] = {0,0,0,0,0,0,0,0,0,0};
     int i;
@@ -104,41 +114,7 @@ int permutation_ab(int a, int b)
     return 1;
 }
 
-int power(int x, int n)
-{
-    if (n<0) return n;
-    if (n==0) return 1;
-    if (n%2 == 0) return power(x*x, n/2);
-    return x*power(x*x,(n-1)/2);
-}
-
-float power_f(float x, int n)
-{
-    if (n<0)
-    {
-        if (x == 0) return x; 
-        x = 1/x;
-        return power_f(x, -n);
-    }
-    if (n==0) return 1;
-    if (n%2 == 0) return power_f(x*x, n/2);
-    return x*power_f(x*x,(n-1)/2);
-}
-
-
-
-int prime(int n)
-{
-    if (n<=1) return 0;
-    if (n==2) return 1;
-    for (int i=2;i<=sqrt(n);i++)
-    {
-        if (n%i==0) return 0;
-    }
-    return 1;
-}
-
-int is_coprime(int a, int b)
+int is_a_coprime_of_b(int a, int b)
 {
     int max, min, div;
     if ((a<=1)||(b<=1)) return 0;
@@ -162,6 +138,48 @@ int is_coprime(int a, int b)
     return 1;
 }
 
+int prime(int n)
+{
+    if (n<=1) return 0;
+    if (n==2) return 1;
+    for (int i=2;i<=sqrt(n);i++)
+    {
+        if (n%i==0) return 0;
+    }
+    return 1;
+}
+
+// arrays, analysis, sorting ...
+
+void display_array(int array[], int size)
+{
+    int i;
+    printf("{");
+    for (i=0;i<size;i++)
+    {
+        printf("%d,", array[i]);
+    }
+    printf("}\n");
+}
+
+void display_array_as_number(int array[], int size)
+{
+    int i;
+    for (i=0;i<size;i++)
+    {
+        printf("%d", array[i]);
+    }
+    printf("\n");
+}
+
+int dichotomy(int array[], int a, int b, int target)
+{
+    int m = (a+b)/2;
+    if (a>b) return -1;
+    if (array[m]>target) return dichotomy(array, a, m-1, target);
+    if (array[m]<target) return dichotomy(array, m+1, b, target);
+    return m;
+}
 
 
 
--- toolbox.h
+++ toolbox.h
@@ -1,27 +1,38 @@
 #ifndef TOOLBOX_H
     #define TOOLBOX_H
 
+// int, numbers and calculations ...
+
 int max(int a, int b);
 unsigned long long max_llu(unsigned long long a, unsigned long long b);
 
 int min(int a, int b);
+<<<<<<< Updated upstream
 unsigned long long min_llu(unsigned long long a, unsigned long long b);
 
+=======
+>>>>>>> Stashed changes
 int count_digits(int n);
+int factorial(int n);
+int power(int x, int n); // returns x^n
+
+// float
+
+float power_f(float x, int n);
+
+// analysis of numbers
+
+int is_n_palindromic(int n); // 1 if n is palindromic, 0 otherwise
+int is_a_permutation_of_b(int a, int b); // 1 if a and b are permutations of each other
+int is_a_coprime_of_b(int a, int b); // 1 if a and b are coprime
+int prime(int n); // 1 if n is prime
+
+// arrays, analysis, sort ...
 
 void display_array(int array[], int size);
 void display_array_as_number(int array[], int size);
-
 int dichotomy(int array[], int a, int b, int target);
 
-int is_a_permutation_of_b(int a, int b); // 1 if a and b are permutations of each other
-int factorial(int n);
-
-int permutation_ab(int a, int b); // 1 if a and b are permutations of each other
-int power(int x, int n); // returns x^n
-float power_f(float x, int n);
-int prime(int n); // 1 if n is prime
-int is_coprime(int a, int b); // 1 if a and b are coprime
 
 // SEQUENCES FUNCTIONS
     // triangular numbers :
